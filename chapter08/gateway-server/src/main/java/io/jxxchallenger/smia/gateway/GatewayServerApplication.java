81ae3ca5b96df74ea754ed3f19a436bcfe8c53b4
jxxchallenger <jxxchallenger@foxmail.com>
2024-10-04 20:23:06 +0800

    Service routing with pring Cloud Gateway

--- chapter08/gateway-server/src/main/java/io/jxxchallenger/smia/gateway/GatewayServerApplication.java
+++ chapter08/gateway-server/src/main/java/io/jxxchallenger/smia/gateway/GatewayServerApplication.java
@@ -2,8 +2,10 @@ package io.jxxchallenger.smia.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
---
+++ chapter08/gateway-server/src/main/java/io/jxxchallenger/smia/gateway/filters/FilterUtils.java
@@ -0,0 +1,30 @@
+package io.jxxchallenger.smia.gateway.filters;
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+@Component
+public class FilterUtils {
+
+    private static final String CORRELATION_ID = "tmx-correlation-id";
+
+    public String getCorrelationId(HttpHeaders requestHeaders) {
+        if (requestHeaders.get(CORRELATION_ID) != null) {
+            List<String> header = requestHeaders.get(CORRELATION_ID);
+            return header.stream().findFirst().get();
+        } else {
+            return null;
+        }
+    }
+
+    public ServerWebExchange setRequestHeader(ServerWebExchange exchange, String name, String value) {
+        return exchange.mutate().request(exchange.getRequest().mutate().header(name, value).build()).build();
+    }
+
+    public ServerWebExchange setCorrelationId(ServerWebExchange exchange, String correlationId) {
+        return this.setRequestHeader(exchange, CORRELATION_ID, correlationId);
+    }
+}
